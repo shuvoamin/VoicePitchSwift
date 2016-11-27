d3189a5d890e4090ada9f344843a7b8ad281c7ea
Shuvo Amin <shuvoamin@shuvos-mbp>
2016-11-27 21:17:18 +0000

    record/stop audio button and helper label for initial view created

--- VoicePitchSwift/VoicePitchSwift/ViewController.swift
+++ VoicePitchSwift/VoicePitchSwift/ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnRecordAudio(_ sender: Any) {
+        print("Record Button Pressed");
+    }
 
 }
 
