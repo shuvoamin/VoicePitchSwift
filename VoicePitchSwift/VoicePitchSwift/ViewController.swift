f80a0a092be5a43f76c3a0b04860e8492457c66e
Shuvo Amin <shuvoamin@shuvos-mbp>
2016-11-27 22:00:47 +0000

    view elements variable added

--- VoicePitchSwift/VoicePitchSwift/ViewController.swift
+++ VoicePitchSwift/VoicePitchSwift/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var RecordAudio: UIButton!
+    @IBOutlet weak var RecordAudioHelperLabel: UILabel!
+    @IBOutlet weak var StopRecordAudio: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
