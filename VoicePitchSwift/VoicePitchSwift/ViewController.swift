705885ec1ff835317125baa71793e9aa97dbb2bf
Shuvo Amin <shuvoamin@shuvos-mbp>
2016-11-28 22:12:59 +0000

    record button helper text changed based on button clicks

--- VoicePitchSwift/VoicePitchSwift/ViewController.swift
+++ VoicePitchSwift/VoicePitchSwift/ViewController.swift
@@ -28,11 +28,13 @@ class ViewController: UIViewController {
     @IBAction func btnRecordAudio(_ sender: Any) {
         RecordAudio.isEnabled = false
         print("Record Button Pressed")
+        RecordAudioHelperLabel.text = "Recording in progress ..."
         StopRecordAudio.isEnabled = true
     }
 
     @IBAction func StopRecordAudio(_ sender: Any) {
         print("Stop Record Button Pressed")
+        RecordAudioHelperLabel.text = "Tap to Record"
         StopRecordAudio.isEnabled = false
         RecordAudio.isEnabled = true
     }
