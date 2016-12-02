7f1578dfbb7d94f58c8b86ec5871a7ab94371f8c
Shuvo Amin <shuvoamin@shuvos-mbp>
2016-12-02 01:03:51 +0000

    compiler warning fixed for filepath

--- VoicePitchSwift/VoicePitchSwift/RecordSoundsViewController.swift
+++ VoicePitchSwift/VoicePitchSwift/RecordSoundsViewController.swift
@@ -33,7 +33,7 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
         let recordingName = "recordedVoice.wav"
         let pathArray = [dirPath, recordingName]
         let filePath = URL(string: pathArray.joined(separator: "/"))
-        print(filePath)
+        print(filePath!)
         
         let session = AVAudioSession.sharedInstance()
         try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, with:AVAudioSessionCategoryOptions.defaultToSpeaker)
